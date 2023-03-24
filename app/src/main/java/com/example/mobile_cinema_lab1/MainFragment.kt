9feea9cb1ebb08b868c212da6cf82abb2aa39f2b
Toloknov Maxim <maxim25072003@gmail.com>
2023-03-24 18:37:54 +0700

    Main frame xml

---
+++ app/src/main/java/com/example/mobile_cinema_lab1/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.mobile_cinema_lab1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.mobile_cinema_lab1.databinding.MainFrameBinding
+import com.example.mobile_cinema_lab1.viewmodels.MainFragmentViewModel
+
+class MainFragment: Fragment() {
+    private lateinit var binding: MainFrameBinding
+
+    private val viewModel by lazy { ViewModelProvider(this)[MainFragmentViewModel::class.java] }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = MainFrameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
